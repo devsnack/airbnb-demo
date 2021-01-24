d09a0b1bd11c2e601500286f227e343024801543
devkit27 <devsnack@gmail.com>
2021-01-24 12:37:46 +0100

    add banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,8 +1,16 @@
-
+import React from 'react';
+import { Button } from '@material-ui/core';
+import "../css/banner.css"
 const Banner = () => {
     return ( 
-        <div>
-            hello
+        <div className="banner__container">
+            <div className="banner__poster">
+                <h5>Find your next destination castle</h5>
+                <p>Créez des expériences mémorables pour
+                 les voyageurs et gagnez de l'argent pour vivre vos
+                  passions.</p>
+                <Button variant="outlined" className="bt__explore">explore more</Button>
+            </div>
         </div>
      );
 }
